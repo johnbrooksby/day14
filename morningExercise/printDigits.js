03a4fe540e0ac9f607a4a133add26bfad5854e5f
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-02 10:20:07 -0700

    after walkthrough

--- morningExercise/printDigits.js
+++ morningExercise/printDigits.js
@@ -1,11 +1,20 @@
+// function printDigits (num) {
+//     num = Math.abs(num);
+//     let numb = 0;
+//     while (num > 0) {
+//         numb = Math.round((((num / 10) - (Math.floor(num / 10))) * 10));
+//         console.log(numb);
+//         num = Math.floor(num / 10);
+//     }
+// }
+
 function printDigits (num) {
     num = Math.abs(num);
-    let numb = 0;
     while (num > 0) {
-        numb = Math.round((((num / 10) - (Math.floor(num / 10))) * 10));
-        console.log(numb);
+        let number = num % 10;
+        console.log(number);
         num = Math.floor(num / 10);
     }
 }
 
-printDigits(314098765786);
+printDigits(31409);
